7598d64d8e75481f61ff73125c76275052495ff1
ethanatortx <ethan@riversfamily.us>
2016-10-08 16:30:04 -0500

    Almost Works (Finish sortBy)

--- csv_reader.cpp
+++ csv_reader.cpp
@@ -51,17 +51,21 @@ vector<vector<string> > readInData(const char* fileName) {
 	
 	for (int outside = 0; outside < data.size(); outside++) {
 		vector<string> vec = data[outside];
-		for (int inside = 0; inside < data.size(); inside++) {
+		for (int inside = 0; inside < vec.size(); inside++) {
 			string item = vec[inside];
-			int strLength = sizeof(item)/sizeof(*item);
+			int strLength = item.length()-1;
 			if (item[0] == '"') {
-				while (item[strLength] != '"') {
-					strLength = sizeof(item)/sizeof(*item);
-					item += vec[inside+1];
-					vec.erase(inside+1);
+				while (!(item[strLength] == '"')) {
+					strLength = item.length()-1;
+					item.append("," + vec[inside+1]);
+					//cout << item[strLength] << " " << strLength << endl;
+					vec.erase(vec.begin()+inside+1);
 				}
+				//cout << item;
+				vec[inside] = item;
 			}
 		}
+		data[outside] = vec;
 	}
 	
 	file.close();
@@ -91,30 +95,121 @@ void writeData(const char* title, const vector<vector<string> > &data) {
 	}
 }
 
-int searchColumn(const vector<vector<string> > &vec, const char* columnHeader) {
+vector<int> getCell(const vector<vector<string> > &vec, const string columnHeader) {
 	
-	int count = -1;
+	int countCol = -1;
+	vector<int> retVal(2,-1);
 	
 	vector<string> tempElement;
 	
 	for (int outside = 0; outside < vec.size(); outside++) {
 		tempElement = vec[outside];
-		if (outside == 16) {
-			for (int inside = 0; inside < tempElement.size(); inside++) {
-				count++;
-				string element(tempElement[inside]);
-				if (element == columnHeader) return count;
+		for (int inside = 0; inside < tempElement.size(); inside++) {
+			countCol++;
+			string element(tempElement[inside]);
+
+			if (element == columnHeader) {
+				retVal[0] = outside;
+				retVal[1] = countCol;
+				return retVal;
 			}
-			count = -1;
 		}
+		countCol = -1;
 	}
-	return count;
+	
+	return retVal;
+}
+
+vector<vector<string> > sortBy(const string columnHeader, const vector<vector<string> > vec, const bool config) {
+	
+	vector<vector<string> > data = vec;
+	vector<int> cell = getCell(data, columnHeader);
+	
+	vector<string> columnData;
+	
+	for (int outside = cell[0]; outside < data.size(); outside++) {
+		columnData.push_back(data[outside][cell[1]]);
+	}
+	
+	vector<double> columnReal;
+	vector<double> sorted;
+	vector<int> sortingIndex;
+	
+	for (int iter = 0; iter < columnData.size(); iter++) {
+		string temp = columnData[iter];
+		stringstream convert(temp);
+		double currentPoint = 0;
+		convert >> currentPoint;
+		
+		//cout << currentPoint << '\n';
+		
+		columnReal.push_back(currentPoint);
+		
+		int inside = 0;
+		
+		while (inside < sorted.size()) {
+			if (currentPoint <= sorted[inside]) {
+				inside++;
+			} else if (currentPoint > sorted[inside]) {
+				break;
+			}
+		}
+		sorted.insert(sorted.begin()+inside, columnReal[iter]);
+		sortingIndex.insert(sortingIndex.begin()+inside, iter);
+		
+		
+	}
+	
+	/*for (int rtg = 0; rtg < sorted.size(); rtg++) {
+		cout << sorted[rtg] << " " << sortingIndex[rtg] << '\n';
+	}*/
+	
+	vector<vector<string> > finalData;
+	
+	int dif = data.size() - sortingIndex.size();
+	
+	for (int finalIter = 0; finalIter < data.size(); finalIter++) {
+		int discrepancy = finalIter - dif;
+		int elementIndex = sortingIndex[discrepancy];
+		if (finalIter > dif) {
+			finalData.push_back(data[elementIndex]);
+			
+		} else {
+			finalData.push_back(data[finalIter]);
+			//cout << '\n';
+		}
+		/*for (int rtg = 0; rtg < data[finalIter].size(); rtg++) {
+			cout << finalData[finalIter][rtg] << " ";
+		}
+		cout << '\n' << elementIndex << " " << discrepancy << '\n';*/
+	}
+	cout << dif;
+	
+	/*for (int rtg = 0; rtg < finalData.size(); rtg++) {
+		vector<string> tempElementLul = finalData[rtg];
+		for (int rtg2; rtg2 < tempElementLul.size(); rtg2++) {
+			cout << tempElementLul[rtg2] << ' ';
+		}
+		cout << '\n';
+	}*/
+	
+	/*for (int rtg = 0; rtg < data.size(); rtg++) {
+		vector<string> tempElementLul = data[rtg];
+		for (int rtg2; rtg2 < tempElementLul.size(); rtg2++) {
+			cout << tempElementLul[rtg2] << ' ';
+		}
+		cout << '\n';
+	}*/
+	
+	return finalData;
 }
 
 int main() {
 	vector<vector<string> > data = readInData("inpfile.csv");
-	writeData("newcsv.csv", data);
+	vector<vector<string> > sorted = sortBy("\"2000-2005\"",data,true);
+	writeData("newcsv.csv", sorted);
 	//cout << searchFor(data,"2000-2005");
-	cout << searchColumn(data,"2000-2005");
+	vector<int> cell = getCell(data,"\"2000-2005\"");
+	vector<vector<string> > vec = sortBy("\"2000-2005\"", data, true);
 	return 0;
 }
