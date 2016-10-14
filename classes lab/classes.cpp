95532acb6ee6001568cf45af4b406446ab3f65ff
ethanatortx <ethan@riversfamily.us>
2016-10-14 11:01:43 -0500

    Classes start

--- classes lab/classes.cpp
+++ classes lab/classes.cpp
@@ -1,30 +1,51 @@
+//Classes
+
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 using namespace std;
 
-struct SALESREC
+class SALESREC
 {
-	char     date[10];
-	char     region[10];
-	char     rep[10];
-	char     item[10];
-	int      units;
-	float    unitCost;
-	float    Total;
+	public:
+		char     date[10];
+		char     region[10];
+		char     rep[10];
+		char     item[10];
+		int      units;
+		float    unitCost;
+		float    Total;
 };
 
-void simpleSortTotal(_____, int c);
+// arguments will be an array of pointers and a count
+void simpleSortTotal( SALESREC* arr[] , int c)
+{
+	for (int i=0; i < c; i++) {
+		for (int j=i; j < c; j++) {
+			if (arr[i]->Total > arr[j]-> Total)//total pointed at by i < total pointed at by j)=
+			{
+				//swap pointer to record I with pointer to record j
+				SALESREC * pointerForI = arr[i];
+				arr[i] = arr[j];
+				arr[j] = pointerForI;
+			}
+		}
+	}
+} 
 
 int main()
 {
 	ifstream infile;
 	int i=0;
 	char cNum[10] ;
-	_______ salesArr[40];
-	int   salesArrayCount;
+	SALESREC salesArr[40];
+	int salesArrayCount;
 	SALESREC* s[40];
 
-		infile.open ("SalesData.txt", ifstream::in);
+		infile.open ("SalesDataP2.csv", ifstream::in);
 		if (infile.is_open())
 		{
 		int   c=0;
@@ -36,9 +57,8 @@ int main()
 			infile.getline(salesArr[c].item, 256, ',');
 			infile.getline(cNum, 256, ',');
 			salesArr[c].units = atoi(cNum);
-			infile.getline(cNum, 256, ',');
+			infile.getline(cNum, 256);
 			salesArr[c].unitCost = atof(cNum);
-			nfile.getline(cNum, 256, '\n');
 			salesArr[c].Total = atof(cNum);
 
 			i++ ;
@@ -54,23 +74,11 @@ int main()
 	
 	// Initialize the sort array pointers
 	for (int i=0; i < salesArrayCount; i++)
-		s[i] = _______________;
+		s[i] = &salesArr[i]; //Trying to insert the values from Sales arr into the pointer array
 	
-	simpleSortTotal (s, salesArrayCount);
+	simpleSortTotal(s, salesArrayCount);
 	  
 	for (int i=0; i < salesArrayCount; i++)
 		cout << "Record: " << s[i]->date <<", " << s[i]->region << ", " << s[i]->rep << ", " << s[i]->item << ", " << s[i]->units << ", " << s[i]->unitCost << ", " << s[i]->Total << endl;
   
 }
- 
-// arguments will be an array of pointers and a count
-void simpleSortTotal( _____ , int c)
-{
-	______
-	for (int i=0; i < c; i++)
-		for (int j=i; j < c; j++)
-		if (//total pointed at by i < total pointed at by j)
-		{
-			//swap pointer to record I with pointer to record j;
-		}
-} 
