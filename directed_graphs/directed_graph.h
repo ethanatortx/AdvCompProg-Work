5d4164aa66b4fbb94f33ccc242fc17189662120b
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-01-10 16:31:42 -0600

    digraph_list vertex structure mark 1

--- directed_graphs/directed_graph.h
+++ directed_graphs/directed_graph.h
@@ -2,54 +2,25 @@
 #define __DIRECTED_GRAPH_H_INCLUDED__
 
 #include "circular_list.h"
-#include <unordered_map>
 #include <functional>
-#include <iterator>
 #include <utility>
 
-template < class Key,
-		   class Hash = std::hash<Key>,
-		   class Pred = std::equal_to<Key>,
-		   class Alloc = std::allocator< std::pair<const Key, List<Key*>>>
-		   >
-class digraph_list 
-{
+template <class T>
+class digraph_list {
 
-	typedef Key* key_type;
-	typedef List<Key*> mapped_type;
-	typedef std::pair<const key_type, mapped_type> value_type;
+	struct Vertex {
+		// pair of one vertex to another
+		typedef std::pair<Vertex*, Vertex*> edge;
 
-	std::unordered_map<key_type, mapped_type, Hash, Pred, Alloc> digraph;
+		// constructor for Vertex
+		Vertex(T&& x, const List<edge> y): data(x), adj(y) {}
 
-public:
+		T data; // data of this vertex
+		List<edge> adj; // nodes adjacent to this node
+	}
 
-	// pre definition for iterators
-	class iterator;
-	class const_iterator;
-
-	digraph_list(); // default initialization constructor
-
-	// copy constructor
-	digraph_list(const digraph_list&); 
-
-	digraph_list& operator=(const digraph_list&);
-	digraph_list& operator=(digraph_list&&);
-
-	List<Key*> operator[](const iterator&) const;
-	List<Key*> operator[](iterator&&) const;
-
-	// get iterator to key
-	iterator& find(Key*);
-
-	// main iterator declaration
-	class iterator {
-		
-	};
-
-	// main constant iterator declaration
-	class const_iterator {};
-
-	~digraph_list();
+	typedef std::pair<std::string, Vertex*> hash;
+	typedef std::set<hash> graph;
 };
 
 template <class T>
