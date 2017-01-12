ca82f58d23263047ce45355e1a0784842ba6c50c
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-01-11 22:32:05 -0600

    hash table structuring and definition

--- template_library/Util/hash/hash_table.cpp
+++ template_library/Util/hash/hash_table.cpp
@@ -1,1 +1,41 @@
-#include "hash.h"
+#include "..\hash_table.h"
+
+template <class T>
+bool hash_table<T>::empty() const {
+
+}
+
+template <class T>
+std::size_t hash_table<T>::size() const {
+
+}
+
+template <class T>
+std::size_t hash_table<T>::max_size() const {
+
+}
+
+template <class T>
+void hash_table<T>::resize(const std::size_t& new_size) {
+
+}
+
+template <class T>
+void hash_table<T>::resize(std::size_t&& new_size) {
+
+}
+
+template <class T>
+void hash_table<T>::add(const T& node) {
+
+}
+
+template <class T>
+void hash_table<T>::add(T&& node) {
+
+}
+
+template <class T>
+void hash_table<T>::clear() {
+	resize(0);
+}
--- template_library/Util/hash_table.h
+++ template_library/Util/hash_table.h
@@ -1,7 +1,7 @@
 #ifndef HASH_H
 #define HASH_H
 
-#include hashing_algorithms\sha-2\sha256
+#include "circular_list.h"
 
 /* hexidecimal ref:
 		ALL hex numbers are preceded by '0x'
@@ -12,11 +12,39 @@
 	1: 1	5: 5	9: 9	D: 13
 	2: 2	6: 6	A: 10	E: 14
 	3: 3	7: 7	B: 11	F: 15
-
-	hex example: 0x18FE92
-	dec convert: 1638034
 */
 
-class hash_table
+template <class T>
+class hash_table {
+
+	struct hash_node {
+		T data;
+		std::string hash;
+	};
+
+	std::size_t bucket_constant;
+	typedef List<hash_node> bucket;
+
+public:
+
+	hash_table(const std::size_t& size = 0, const std::size_t& bucket_const = 0): bucket_const(bucket_const) 
+	{
+		table[size];
+	}
+	hash_table(const hash_table&);
+
+	bool empty() const;
+
+	std::size_t size() const;
+	std::size_t max_size() const;
+
+	void resize(const std::size_t&);
+	void resize(std::size_t&&);
+
+	void add(const T&);
+	void add(T&&);
+
+	void clear();
+};
 
 #endif
