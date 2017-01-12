8903406f281687257c4561f62cc46befa4c2b57e
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-01-11 18:27:33 -0600

    directed graph adjacency list structuring and planning

--- template_library/directed_graph/directed_graph.h
+++ template_library/directed_graph/directed_graph.h
@@ -1,35 +1,51 @@
-#ifndef __DIRECTED_GRAPH_H_INCLUDED__
-#define __DIRECTED_GRAPH_H_INCLUDED__
+#ifndef DIRECTED_GRAPH_H
+#define DIRECTED_GRAPH_H
 
 #include "circular_list.h"
+#include "hash_table.h"
 #include <functional>
+#include <iterator>
 #include <utility>
 
 template <class T>
 class digraph_list {
 
-	struct Vertex;
+	typedef List<T> edges;
+	typedef std::string hash;
 
-	typedef Vertex* pointer;
-	typedef Vertex& reference;
-	typedef const Vertex& const_reference;
-
-	
+	hash_table<T> index = new hash_table();
 
 public:
 
+	class iterator: public std::iterator<std::forward_iterator_tag, edges >;
+	class const_iterator: public std::iterator<std::forward_iterator_tag, edges >;
 
+	digraph_list();
+	digraph_list(const digraph_list&);
+	~digraph_list();
 
+	digraph_list<T>& operator=(const digraph_list<T>&);
+	digraph_list<T>& operator=(digraph_list<T>&&);
+
+	void emplace(const T&);
+	void emplace(T&&);
+
+	iterator emplace(const T&);
+	iterator emplace(T&&);
+
+	void erase(const T&);
+	void erase(T&&);
+
+	iterator& find(const T&);
+	iterator& find(T&&);
+
+	iterator& operator[](const T&);
+	iterator& operator[](T&&);
 };
 
 template <class T>
 class dirgraph_matrix {
-
-
-
 public:
-
-
 };
 
 #endif
