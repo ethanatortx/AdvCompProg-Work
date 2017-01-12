f89dff36c7d42c77489e42f717079e06278f9353
DESKTOP-DAL922I\Ethan Rivers <ethan@riversfamily.us>
2017-01-12 16:55:55 -0600

    hash table update

--- template_library/Util/hash/hash_table.cpp
+++ template_library/Util/hash/hash_table.cpp
@@ -1,41 +1,16 @@
 #include "..\hash_table.h"
 
+
 template <class T>
-bool hash_table<T>::empty() const {
+void hash_table<T>::reallocate(const std::size_t& new_bucket_count) 
+{
+	typename hash_table<T>::table new_tbl = new typename hash_table<T>::table();
 
 }
 
 template <class T>
-std::size_t hash_table<T>::size() const {
-
-}
-
-template <class T>
-std::size_t hash_table<T>::max_size() const {
-
-}
-
-template <class T>
-void hash_table<T>::resize(const std::size_t& new_size) {
-
-}
-
-template <class T>
-void hash_table<T>::resize(std::size_t&& new_size) {
-
-}
-
-template <class T>
-void hash_table<T>::add(const T& node) {
-
-}
-
-template <class T>
-void hash_table<T>::add(T&& node) {
-
-}
-
-template <class T>
-void hash_table<T>::clear() {
-	resize(0);
+hash_table<T>::hash_table(const hash_table<T>& H) 
+{
+	this->bucket_constant = H.bucket_constant;
+	this->table = H.table;
 }
--- template_library/Util/hash_table.h
+++ template_library/Util/hash_table.h
@@ -1,8 +1,11 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <vector>
+#include <iterator>
+#include <functional>
 #include "circular_list.h"
-
+#include "hash\hashing_algorithms\sha-2\sha256\sha256.h"
 /* hexidecimal ref:
 		ALL hex numbers are preceded by '0x'
 		This indicates it is a hex number to
@@ -14,29 +17,49 @@
 	3: 3	7: 7	B: 11	F: 15
 */
 
-template <class T>
+template <class T,
+		  class Hash = std::hash<T>,
+		  class Alloc = std::allocator<T> >
 class hash_table {
 
 	struct hash_node {
+		hash_node(const T& x, const std::string& y = 0): data(x), hash(y) {}
 		T data;
 		std::string hash;
 	};
 
+	typedef List<typename 	hash_table<T>::hash_node> bucket;
+	typedef typename std::vector<typename hash_table<T>::bucket> h_table;
+
 	std::size_t bucket_constant;
-	typedef List<hash_node> bucket;
+	typename hash_table<T>::h_table table;
+
+	void reallocate(const std::size_t&);
 
 public:
 
-	hash_table(const std::size_t& size = 0, const std::size_t& bucket_const = 0): bucket_const(bucket_const) 
-	{
-		table[size];
-	}
+	class iterator;
+	class const_iterator;
+
+	typedef typename hash_table<T>::bucket value_type;
+	typedef std::size_t size_type;
+	typedef std::ptrdiff_t difference_type;
+
+
+	hash_table(const std::size_t& bucket_constant = 0): 
+		bucket_constant(bucket_constant) {}
 	hash_table(const hash_table&);
 
-	bool empty() const;
+	inline bool empty() const { return this->table.empty(); }
 
-	std::size_t size() const;
-	std::size_t max_size() const;
+	inline std::size_t size() const { return this->table.size(); }
+	inline std::size_t max_size() const { return this->table.max_size(); }
+
+	inline void set_bucket_const(const std::size_t& b_const) { this->bucket_constant = b_const; }
+	inline void set_bucket_const(std::size_t&& b_const) { this->bucket_constant = b_const; }
+	inline std::size_t get_bucket_const() const { return bucket_constant; }
+
+	inline void clear() { this->table.clear(); }
 
 	void resize(const std::size_t&);
 	void resize(std::size_t&&);
@@ -44,7 +67,29 @@ public:
 	void add(const T&);
 	void add(T&&);
 
-	void clear();
+	~hash_table();
+
+	class iterator: public std::iterator<std::random_access_iterator_tag, typename hash_table<T>::bucket> {
+
+
+	public:
+		friend class hash_table;
+		friend class const_iterator;
+
+		typedef typename hash_table<T>::value_type value_type;
+		typedef typename hash_table<T>::value_type& reference;
+		typedef typename hash_table<T>::value_type* pointer;
+	};
+
+	class const_iterator: public std::iterator<std::random_access_iterator_tag, typename hash_table<T>::bucket> {
+
+
+	public:
+		friend class hash_table;
+		friend class iterator;
+		
+
+	};
 };
 
 #endif
